5ec47b85f6f0b3ba5aeb46d760908832d99482b0
Murilo Portugal <murilo.portugal@gmail.com>
2022-05-03 21:51:23 -0300

    teste2

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -7,7 +7,7 @@ export function Widget(){
     return (
         <Popover className='absolute bottom-5 right-5'>
             
-            <Popover.Panel>Hello World!</Popover.Panel>
+            <Popover.Panel>Hello World</Popover.Panel>
             
             <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
                 <ChatTeardropDots className='w-6 h-6'/>
